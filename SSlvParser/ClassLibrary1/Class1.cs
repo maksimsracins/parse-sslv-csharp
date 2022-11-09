b6132e3726365b285aca54a0a22c3855360eed36
Maksims Racins <maksims.racins@gmail.com>
2022-11-09 23:36:20 +0200

    sslv

---
+++ SSlvParser/ClassLibrary1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using HtmlAgilityPack;
+
+namespace ClassLibrary1;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        getHtmlAsync();
+        Console.ReadLine();
+    }
+
+    private static async void getHtmlAsync()
+    {
+        string url = "https://www.ss.lv/ru/transport/cars/audi/";
+        HttpClient httpClient = new HttpClient();
+        var html = await httpClient.GetStringAsync(url);
+
+        var htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml(html);
+
+        var ProductList = htmlDocument.DocumentNode.Descendants("tr")
+            .Where(node => node.GetAttributeValue("id", "")
+            .Equals("tr_52357345")).ToList();
+
+        Console.WriteLine(ProductList);
+
+    }
+
+}
